591ef4facaadd1e9d05a53f6b240cadde2424ed3
Kelvin-Ben <kevoben14@gmail.com>
2023-02-25 10:02:31 +0300

    Add validations to a few models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ApplicationRecord
     has_many :category_records, dependent: :delete_all
-  has_many :records, through: :category_records, dependent: :delete_all
+    has_many :records, through: :category_records, dependent: :delete_all
+    has_one_attached :image
+
+    validates :name, presence: true
+    validates :user, presence: true
 
   def total_amount
     records.sum('amount')
--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,7 @@ class Record < ApplicationRecord
     belongs_to :user
     has_many :category_records, dependent: :delete_all
     has_many :categories, through: :category_records
+
+    validates :name, presence: true
+    validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
